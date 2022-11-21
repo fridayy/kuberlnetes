43589990ace92a89b89d2d4be259a7002aebe24c
fridayy# <benjamin.krenn@leftshift.one>
2022-11-21 20:28:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+kuberlnetes
+=====
+
+An OTP library
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/kuberlnetes.erl
@@ -0,0 +1,146 @@
+%%%=============================================================================
+%%% @doc kuberlnetes
+%%% A low level kubernetes api helper - primarlily for applications that interact
+%%% with the kubernetes api from inside the cluster.
+%%% @end
+%%%=============================================================================
+-module(kuberlnetes).
+
+-export([in_cluster/0, get/2, post/2, patch/2, microtime_now/0]).
+
+-author("bnjm").
+
+-type options() :: #{
+    server => server() | undefined
+}.
+
+-type mutation_request() :: #{
+    path => string(),
+    body => map()
+}.
+
+
+-record(auth_token, {token :: binary()}).
+-record(server, {
+    url :: string(),
+    ca_cert :: string(),
+    auth :: #auth_token{},
+    skip_tls_verify :: boolean()
+}).
+
+-type server() :: #server{}.
+
+%% @doc
+%% Configures a server using the service account
+%% @end
+-spec in_cluster() -> server().
+in_cluster() ->
+    Host = os:getenv("KUBERNETES_SERVICE_HOST"),
+    Port = os:getenv("KUBERNETES_SERVICE_PORT"),
+    {ok, Token} = file:read_file("/var/run/secrets/kubernetes.io/serviceaccount/token"),
+    #server{
+        url = "https://" ++ Host ++ ":" ++ Port,
+        ca_cert = cert_from_file("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"),
+        auth = #auth_token{token = Token}
+    }.
+
+%% @doc
+%% Initiates a GET request against the configured kubernetes api server
+%% or raises an error if the server is not configured correctly.
+%% @end
+-spec get(Path, Opts) -> Body when
+    Path :: string(),
+    Opts :: options(),
+    Body :: map().
+get(Path, Opts) ->
+    Server = get_server(Opts),
+    {ok, {{_, 200, _}, _, Body}} = httpc:request(
+        get,
+        {Server#server.url ++ Path, headers(Server)},
+        [{ssl, [{cacerts, [Server#server.ca_cert]}]}],
+        []
+    ),
+    decode(Body).
+
+%% @doc
+%% Initiates a POST request against the configured kubernetes api server or
+%% raises an error if the server is not configured correctly.
+%% see: server()
+%% @end
+-spec post(Request, Opts) -> ok when
+    Request :: mutation_request(),
+    Opts :: options().
+post(#{path := Path, body := Body}, Opts) ->
+    Server = get_server(Opts),
+    {ok, {{_, 201, _}, _, _}} = httpc:request(
+        post,
+        {Server#server.url ++ Path, headers(Server), "application/json", jsone:encode(Body)},
+        [{ssl, [{cacerts, [Server#server.ca_cert]}]}],
+        []
+    ),
+    ok.
+
+%% @doc
+%% Initiates a PATCH request against the configured kubernetes api server or
+%% raises an error if the server is not configured correctly.
+%% see: server()
+%% @end
+-spec patch(Request, Opts) -> ok when
+    Request :: mutation_request(),
+    Opts :: options().
+patch(#{path := Path, body := Body}, Opts) ->
+    Server = get_server(Opts),
+    {ok, {{_, 200, _}, _, _}} = httpc:request(
+        patch,
+        {
+            Server#server.url ++ Path,
+            headers(Server),
+            "application/merge-patch+json",
+            jsone:encode(Body)
+        },
+        [{ssl, [{cacerts, [Server#server.ca_cert]}]}],
+        []
+    ),
+    ok.
+
+%% @doc
+%% Returns the current datetime in the kubernetes MicroTime format
+%% see: https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.22/#microtime-v1-meta
+%% @end
+-spec microtime_now() -> binary().
+microtime_now() ->
+    erlang:list_to_binary(
+        calendar:system_time_to_rfc3339(erlang:system_time(microsecond), [
+            {unit, microsecond}, {offset, "Z"}
+        ])
+    ).
+
+%% internal functions
+headers(Server) -> headers(Server, []).
+headers(#server{auth = #auth_token{token = Token}}, AdditionalHeaders) ->
+    [
+        {"Accept", "application/json"},
+        {"Authorization", "Bearer " ++ Token}
+    ] ++ AdditionalHeaders.
+
+cert_from_file(Path) ->
+    true = filelib:is_file(Path),
+    {ok, BinaryContent} = file:read_file(Path),
+    [{'Certificate', Data, _}] = public_key:pem_decode(BinaryContent),
+    Data.
+
+cert_from_b64(CertData) when is_list(CertData) ->
+    DecodedCert = base64:decode(CertData),
+    [{'Certificate', Data, _}] = public_key:pem_decode(DecodedCert),
+    Data.
+
+get_server(Opts) ->
+    OptsServer = maps:get(server, Opts, undefined),
+    case OptsServer of
+        undefined -> error(no_server_configured);
+        Else when is_record(Else, server) -> Else;
+        _ -> error(invalid_server)
+    end.
+
+decode(Body) when is_binary(Body) -> jsone:decode(Body);
+decode(Body) when is_list(Body) -> jsone:decode(erlang:list_to_binary(Body)).
